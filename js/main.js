1765e4b05db92e8b44d80ea25d81bc0876ca7047
Romain Vigo Benia <romain.vigobenia@gmail.com>
2020-12-26 11:53:44 +0100

    update wedding date

--- js/main.js
+++ js/main.js
@@ -252,7 +252,7 @@
 
 
 	// Set the date we're counting down to
-		var countDownDate = new Date("Jul 31, 2020 15:00:00").getTime();
+		var countDownDate = new Date("Aug 1, 2021 15:00:00").getTime();
 
 		// Update the count down every 1 second
 		var x = setInterval(function() {
@@ -280,10 +280,10 @@
 		document.getElementById("seconds").innerHTML = seconds + " <small>seconds</small> ";
 
 		// If the count down is finished, write some text
-		if (distance < 0) {
-		 clearInterval(x);
-		 document.getElementById("demo").innerHTML = "The Wedding Ceremony is Over";
-		}
+		// if (distance < 0) {
+		//  clearInterval(x);
+		//  document.getElementById("demo").innerHTML = "The Wedding Ceremony is Over";
+		// }
 		}, 1000);
 
 
